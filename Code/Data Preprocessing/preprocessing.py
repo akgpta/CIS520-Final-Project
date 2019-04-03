c4614e88b1d5d47efd316638c851f81fc3b03b7b
Aashish Jain <aashj99@gmail.com>
2019-04-02 21:31:41 -0400

    Cleaned preprocessing script

--- Code/Data Preprocessing/preprocessing.py
+++ Code/Data Preprocessing/preprocessing.py
@@ -3,11 +3,18 @@ import pandas as pd
 
 def main():
     suicide_rates = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/suicide_rates_pivot.csv", index_col=None)
-    suicide_rates = clean_suicide_rates(suicide_rates)
     fertility_rates = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/fertility_rate.csv", index_col=None)
-    fertility_rates = clean_fertility_rates(fertility_rates, countries=suicide_rates['Country'])
-    drinks = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/drinks.csv", index_col=None)
-    print(drinks);
+    drinks = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/alcohol_consumption.csv", index_col=None)
+    life_expectancy = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/life_expectancy.csv", index_col=None)
+    country_population = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/country_population.csv", index_col=None)
+    youth_unemployment = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/youth_unemployment.csv", index_col=None)
+
+    suicide_rates = clean_dataframe(suicide_rates)
+    fertility_rates = clean_dataframe(fertility_rates)
+    drinks = clean_dataframe(drinks, country_column='country')
+    life_expectancy = clean_dataframe(life_expectancy)
+    country_population = clean_dataframe(country_population)
+    youth_unemployment = clean_dataframe(youth_unemployment)
 
 
 def clean_country_name(country_name):
@@ -15,26 +22,11 @@ def clean_country_name(country_name):
     return country_name
 
 
-def clean_fertility_rates(fertility_rate_dataframe, countries=[]):
-    fertility_rate_dataframe['Country'] = fertility_rate_dataframe['Country'].astype(str);
-    fertility_rate_dataframe.sort_values(by=['Country'])
-    fertility_rate_dataframe['Country'] = fertility_rate_dataframe['Country'].apply(clean_country_name);
-    fertility_rate_countries = fertility_rate_dataframe['Country'];
-    dropped_countries = []
-    for country in fertility_rate_countries:
-        if (country not in countries):
-            dropped_countries.append(country);
-
-    #fertility_rate_dataframe = fertility_rate_dataframe[~fertility_rate_dataframe['Country'].isin(dropped_countries)]
-
-    return fertility_rate_dataframe
-
-
-
-def clean_suicide_rates(suicide_rate_dataframe):
-    suicide_rate_dataframe.sort_values(by=['Country'])
-    suicide_rate_dataframe['Country'] = suicide_rate_dataframe['Country'].apply(clean_country_name);
-    return suicide_rate_dataframe
+def clean_dataframe(dataframe, country_column='Country'):
+    dataframe[country_column] = dataframe[country_column].astype(str);
+    dataframe[country_column] = dataframe[country_column].apply(clean_country_name)
+    dataframe.sort_values(by=[country_column])
+    return dataframe;
 
 
 if __name__ == "__main__":
