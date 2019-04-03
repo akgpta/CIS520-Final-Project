cc006be379cb46d6dc083ff0cf2853000e5b1903
Aashish Jain <aashj99@gmail.com>
2019-04-02 21:44:27 -0400

    Added merging countries

--- Code/Data Preprocessing/preprocessing.py
+++ Code/Data Preprocessing/preprocessing.py
@@ -9,12 +9,18 @@ def main():
     country_population = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/country_population.csv", index_col=None)
     youth_unemployment = pd.DataFrame.from_csv("../../Data/Manually-Cleaned/youth_unemployment.csv", index_col=None)
 
+    countries = suicide_rates['Country'].tolist();
     suicide_rates = clean_dataframe(suicide_rates)
-    fertility_rates = clean_dataframe(fertility_rates)
-    drinks = clean_dataframe(drinks, country_column='country')
-    life_expectancy = clean_dataframe(life_expectancy)
-    country_population = clean_dataframe(country_population)
-    youth_unemployment = clean_dataframe(youth_unemployment)
+    fertility_rates = clean_dataframe(fertility_rates, countries=countries)
+    drinks = clean_dataframe(drinks, country_column='country', countries=countries)
+    life_expectancy = clean_dataframe(life_expectancy, countries=countries)
+    country_population = clean_dataframe(country_population, countries=countries)
+    youth_unemployment = clean_dataframe(youth_unemployment, countries=countries)
+    print(fertility_rates)
+
+
+
+    create_Y(suicide_rates)
 
 
 def clean_country_name(country_name):
@@ -22,12 +28,36 @@ def clean_country_name(country_name):
     return country_name
 
 
-def clean_dataframe(dataframe, country_column='Country'):
+def clean_dataframe(dataframe, country_column='Country', countries=[]):
     dataframe[country_column] = dataframe[country_column].astype(str);
     dataframe[country_column] = dataframe[country_column].apply(clean_country_name)
     dataframe.sort_values(by=[country_column])
+    dataframe = merge_countries(dataframe, country_column_name=country_column, countries=countries)
     return dataframe;
 
+
+def merge_countries(dataframe, country_column_name='Country', countries=[]):
+    dataframe_countries = dataframe[country_column_name]
+    dropped_countries = []
+    for country in dataframe_countries:
+        if country not in countries.tolist():
+            dropped_countries.append(country)
+
+    dataframe = dataframe[~dataframe[country_column_name].isin(dropped_countries)]
+    return dataframe
+
+
+def create_Y(dataframe):
+    df = dataframe['2010']
+    df.to_csv("../../Data/Cleaned/Y_2010.csv")
+    df = dataframe['2011']
+    df.to_csv("../../Data/Cleaned/Y_2011.csv")
+    df = dataframe['2012']
+    df.to_csv("../../Data/Cleaned/Y_2012.csv")
+    df = dataframe['2013']
+    df.to_csv("../../Data/Cleaned/Y_2013.csv")
+    df = dataframe['2014']
+    df.to_csv("../../Data/Cleaned/Y_2014.csv")
 
 if __name__ == "__main__":
     main()
